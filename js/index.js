e388d5ebad1fd5aaad9b41d60e064df392ab9f9d
puneeth sai <63712689+puneeth-sai-k@users.noreply.github.com>
2020-11-23 16:45:34 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,17 @@
+///////////////// fixed menu on scroll for desktop
+if ($(window).width() > 100) {
+  $(window).scroll(function(){
+     if ($(this).scrollTop() > 40) {
+        $('#navbar_top').addClass("fixed-top");
+        // add padding top to show content behind navbar
+        $('body').css('padding-top', $('.navbar').outerHeight() + 'px');
+      }else{
+        $('#navbar_top').removeClass("fixed-top");
+         // remove padding top from body
+        $('body').css('padding-top', '0');
+      }
+  });
+}
 
 
 var d = new Date().getFullYear();
